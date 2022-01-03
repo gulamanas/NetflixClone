25ca8ea13bc88abed9308a016d1ecbc872e2aea2
Gulam Anas <g.anas786@gmail.com>
2022-01-03 22:19:25 +0530

    Initial repos Netflix clone

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <div className='brand'>
+                NETFLIX
+            </div>
+            <ul>
+                <li>English</li>
+                <li>Sign In</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
