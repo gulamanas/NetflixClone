8f8ea5b55cbd71f6941ff72d4201b40fa6fac5ba
Gulam Anas <g.anas786@gmail.com>
2022-01-04 23:09:45 +0530

    images updated for netflix

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import axios from './axios';
 import React, { useState, useEffect } from 'react';
 
+const base_url = "https://image.tmdb.org/t/p/original/"
+
 function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
 
@@ -8,11 +10,13 @@ function Row({ title, fetchUrl }) {
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl)
-            console.log(request)
+            console.log(request.data.results)
+            setMovies(request.data.results);
         }
         fetchData();
         // if [], run once when the row loads, and don't run again
-    }, []);
+    }, [fetchUrl]);
+    console.table(movies)
 
     return (
         <div>
@@ -20,6 +24,13 @@ function Row({ title, fetchUrl }) {
             <h2>{title}</h2>
 
             {/* Container -> posters */}
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
+
+
         </div>
     )
 }
