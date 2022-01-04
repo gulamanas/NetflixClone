9e7085a6e8bb52df1f2c1d752636b44593cd2dcd
Gulam Anas <g.anas786@gmail.com>
2022-01-04 22:46:06 +0530

    fetching data using async await

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Row from './Row';
+import requests from './request';
 
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINAL" />
-      <Row title="something I do" />
+      <Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginal} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';
+import axios from './axios';
+import React, { useState, useEffect } from 'react';
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
+
+    // A snippet of code which runs based on a specific condition
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl)
+            console.log(request)
+        }
+        fetchData();
+        // if [], run once when the row loads, and don't run again
+    }, []);
 
     return (
         <div>
+            {/* Title */}
             <h2>{title}</h2>
+
+            {/* Container -> posters */}
         </div>
     )
 }
