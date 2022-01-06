f9db66ca06fd30e4a7b5b50a5ec4603c79360d10
Gulam Anas <g.anas786@gmail.com>
2022-01-06 22:39:10 +0530

    banner finished

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import requests from './request';
+import './Banner.css'
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -18,30 +19,31 @@ function Banner() {
 
     console.log(movie)
 
+    function truncate(str, n) {
+        return str?.length > n ? str.sunstr(0, n - 1) + "..." : str;
+    }
+
     return (
         <header className='banner'
             style={{
                 backgroundSize: 'cover',
                 backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-                backgroundPosition: "center center"
+                backgroundPosition: "center center",
             }}
         >
             <div className="banner_contents">
-                <h1>
+                <h2 className='banner_title'>
                     {movie?.title || movie?.name || movie?.original_name}
-                </h1>
+                </h2>
                 <div className="banner_buttons">
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My List</button>
                 </div>
                 <p className='banner_description'>
-                    {movie?.overview}
+                    {truncate(movie?.overview)}
                 </p>
             </div>
-
-            {/* title */}
-            {/* div > 2 buttons */}
-            {/* description */}
+            <div className="banner_fadeBottom"></div>
         </header>
     )
 }
