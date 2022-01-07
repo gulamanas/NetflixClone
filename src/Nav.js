7beccc2edd2d011b7cd70480795ff815adce5761
Gulam Anas <g.anas786@gmail.com>
2022-01-07 23:28:20 +0530

    finished building navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import '.Nav.css'
+import React, { useEffect, useState } from 'react'
+import './Nav.css'
 import logo from './images/netflix_logo.png'
 import avatar from './images/Netflix-avatar.png'
 
 function Nav() {
+    const [show, handleShow] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener('scroll')
+        }
+    }, []);
+
+
     return (
-        <div className='nav'>
-            <img src={logo} alt="Netflix Logo" />
-            <img src={avatar} alt="Netflix avatar" />
+        <div className={`nav ${show && "nav_black"}`}>
+            <img className='nav_logo' src={logo} alt="Netflix Logo" />
+            <img className='nav_avatar' src={avatar} alt="Netflix avatar" />
         </div>
     )
 }
