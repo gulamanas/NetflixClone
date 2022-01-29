8e455d357df6b4c223f904e37bcea4bb0a34bc44
Gulam Anas <g.anas786@gmail.com>
2022-01-29 23:37:24 +0530

    added env file

--- src/request.js
+++ src/request.js
@@ -1,4 +1,4 @@
-const API_KEY = 'fc1f3f00fc5ade5abe25aefedd1a71e4';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
