34433cbd13308c7d13e22a7bfaaffd350df7fa30
Gulam Anas <g.anas786@gmail.com>
2022-03-30 23:17:52 +0530

    added netflix backlink

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import './App.css';
-import Row from './Row';
-import requests from './request';
-import Banner from './Banner';
-import Nav from './Nav';
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
+import Banner from "./Banner";
+import Nav from "./Nav";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Nav />
       <Banner />
       <Row
-        title="NETFLIX ORIGINALS"
+        title='NETFLIX ORIGINALS'
         fetchUrl={requests.fetchNetflixOriginal}
-        isLargeRow />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+        isLargeRow
+      />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>
+        This is an inspiration site and used the Netflix original sites design
+        for learning purpose only
+      </p>
+      <p>
+        All credit &copy; to the original owner &nbsp;
+        <a href='https://www.netflix.com/'>Netflix</a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
